1c7d84053c9669556a65c96c421a71d41e572939
unknown <vrushalisk10092002@gmail.com>
2026-08-04 18:08:14 +0530

    changed the entity name in the service repository and the password to root in application properties

--- FlexServ Backend/SpringBoot/AdminService/src/main/java/com/flexserv/repository/ServiceRepository.java
+++ FlexServ Backend/SpringBoot/AdminService/src/main/java/com/flexserv/repository/ServiceRepository.java
@@ -1,9 +1,9 @@
 package com.flexserv.repository;
 
-import com.flexserv.entity.ServiceEntity;
+import com.flexserv.entity.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ServiceRepository extends JpaRepository<ServiceEntity, Long> {
+public interface ServiceRepository extends JpaRepository<Service, Long> {
 }
