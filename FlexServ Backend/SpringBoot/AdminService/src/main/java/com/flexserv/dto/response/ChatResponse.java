48467e15fe7f127f5eff61c263fbb1f58f7e4691
Shreyas Hedau <hedaushreyas@gmail.com>
2026-08-07 19:47:44 +0530

    Add chatbot request and response DTOs

---
+++ FlexServ Backend/SpringBoot/AdminService/src/main/java/com/flexserv/dto/response/ChatResponse.java
@@ -0,0 +1,10 @@
+package com.flexserv.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ChatResponse {
+    private String reply;
+}
