db45ff262a63bfec467680b31a504870213466d3
Shreyas Hedau <hedaushreyas@gmail.com>
2026-08-08 02:03:22 +0530

    Extend service provider repository

--- FlexServ Backend/SpringBoot/AdminService/src/main/java/com/flexserv/repository/ServiceProviderRepository.java
+++ FlexServ Backend/SpringBoot/AdminService/src/main/java/com/flexserv/repository/ServiceProviderRepository.java
@@ -1,8 +1,10 @@
 package com.flexserv.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.flexserv.entity.ServiceProvider;
 
 public interface ServiceProviderRepository extends JpaRepository<ServiceProvider, Long> {
+    Optional<ServiceProvider> findByUserId(Long userId);
 }
